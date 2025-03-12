77c886d52237ecb07c0a81914ba8f3639bf451ba
hongyinull <80037820+hongyinull@users.noreply.github.com>
2025-03-12 22:49:07 +0800

    Initial Commit

---
+++ onair/onairApp.swift
@@ -0,0 +1,17 @@
+//
+//  onairApp.swift
+//  onair
+//
+//  Created by HONGYINULL on 2025/3/12.
+//
+
+import SwiftUI
+
+@main
+struct onairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
